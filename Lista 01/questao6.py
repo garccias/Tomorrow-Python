a5b0a88f518cfab4b9a5af0e97bbc55fb44c3d2f
garcia <sophiagarccia2@gmail.com>
2025-04-08 11:11:51 -0300

    enviando a questao 6 e 7

---
+++ Lista 01/questao6.py
@@ -0,0 +1,9 @@
+# 6. Peça um número ao usuário e exiba o dobro, o triplo e a raiz quadrada dele.
+
+numero = int(input("Digite um numero:\n"))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** 0.5
+
+print(f"O dobro é {dobro}, o triplo {triplo} e a raiz {raiz}")
---
+++ Lista 01/questao7.py
@@ -0,0 +1,9 @@
+# 7. Peça três notas ao usuário e calcule a média.
+
+num1 = float(input("Digite a nota 1:\n"))
+num2 = float(input("Digite a nota 2:\n"))
+num3 = float(input("Digite a nota 3:\n"))
+
+media = (num1 + num2 + num3) / 3.0
+
+print(f"A media é {media}")
